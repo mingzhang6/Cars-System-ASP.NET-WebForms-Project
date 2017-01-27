556f61ed0d5fb4d37a65503576d149e2cf6b950a
todor-enikov <enigmaenikov@gmail.com>
2017-01-27 16:38:41 +0200

    Installed entity framework.Created class CarsSystemDbContext. Enabled migrations and configurated in Configuration.cs class.

---
+++ Source/CarsSystem/CarsSystem.Data/CarsSystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CarsSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsSystem.Data
+{
+    public class CarsSystemDbContext : DbContext
+    {
+        public CarsSystemDbContext()
+            : base("CarsSystem")
+        {
+
+        }
+
+        public virtual IDbSet<User> Users { get; set; }
+
+        public virtual IDbSet<Car> Cars { get; set; }
+    }
+}
