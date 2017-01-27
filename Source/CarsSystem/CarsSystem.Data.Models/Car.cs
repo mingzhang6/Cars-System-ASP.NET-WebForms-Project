bf672436d73912ae2356083a3ad57705ab47652b
todor-enikov <enigmaenikov@gmail.com>
2017-01-27 16:36:18 +0200

    Added models for the Database.

---
+++ Source/CarsSystem/CarsSystem.Data.Models/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsSystem.Data.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public EngineType TypeOfEngine { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public string VINNumber { get; set; }
+
+        public byte CountOfTyres { get; set; }
+
+        public byte CountOfDoors { get; set; }
+
+        public CarType TypeOfCar { get; set; }
+
+        public DateTime YearOfManufacturing { get; set; }
+
+        public DateTime ValidUntilAnnualCheckUp { get; set; }
+
+        public DateTime ValidUntilVignette { get; set; }
+
+        public DateTime ValidUntilInsurance { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Source/CarsSystem/CarsSystem.Data.Models/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsSystem.Data.Models
+{
+    public class User
+    {
+        private ICollection<Car> cars;
+
+        public User()
+        {
+            this.cars = new HashSet<Car>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string LastName { get; set; }
+
+        public long EGN { get; set; }
+
+        public int NumberOfIdCard { get; set; }
+
+        public DateTime DateOfIssue { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public Role Role { get; set; }
+
+        public virtual ICollection<Car> Cars
+        {
+            get { return this.cars; }
+            set { this.cars = value; }
+        }
+    }
+}
