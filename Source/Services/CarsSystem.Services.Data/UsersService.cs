831126d3e91b45f27d9b57a367dddf35bd7c3c54
todor-enikov <enigmaenikov@gmail.com>
2017-02-13 15:04:05 +0200

    Implemented UsersService class.

--- Source/Services/CarsSystem.Services.Data/UsersService.cs
+++ Source/Services/CarsSystem.Services.Data/UsersService.cs
@@ -1,4 +1,6 @@
-﻿using CarsSystem.Services.Data.Contracts;
+﻿using CarsSystem.Data.Models;
+using CarsSystem.Data.Repositories;
+using CarsSystem.Services.Data.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,26 @@ namespace CarsSystem.Services.Data
 {
     public class UsersService : IUsersService
     {
+        private readonly IRepository<User> userRepo;
+
+        public UsersService(IRepository<User> userRepo)
+        {
+            if (userRepo == null)
+            {
+                throw new ArgumentException("The user repo shold not be null!");
+            }
+
+            this.userRepo = userRepo;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return this.userRepo.All();
+        }
+
+        public User GetUserById(string id)
+        {
+            return this.userRepo.GetById(id);
+        }
     }
 }
