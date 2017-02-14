abd55cbf8c9c7c99c6f7a58da26c34ac143bb51d
todor-enikov <enigmaenikov@gmail.com>
2017-02-14 16:34:51 +0200

    Used DatabaseConfig in Global.asax to initialize the database on start of the application.

--- Source/CarsSystem.WebForms.Client/Global.asax.cs
+++ Source/CarsSystem.WebForms.Client/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarsSystem.WebForms.Client.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,7 @@ namespace CarsSystem.WebForms.Client
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DatabaseConfig.Initialize();
         }
     }
 }
