9d0d3d27b156b6990feaff5def63244346874ef7
todor-enikov <enigmaenikov@gmail.com>
2017-02-14 20:54:48 +0200

    Added Moq nuget package and created few test for the EfGenericRepository class.

---
+++ Source/CarsSystem.Tests/Data/CarsSystem.Data/EfGenericRepository.Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+using CarsSystem.Data;
+using CarsSystem.Data.Repositories;
+using CarsSystem.Data.Models;
+using Moq;
+
+namespace CarsSystem.Tests.Data.CarsSystem.Data
+{
+    [TestFixture]
+    public class UnitTest2
+    {
+        [Test]
+        public void EfGenericRepository_ShouldThrowArgumentNullException_WhenThePassedContextIsNull()
+        {
+            ICarsSystemDbContext context = null;
+
+            Assert.Throws<ArgumentException>(() => new EfGenericRepository<Car>(context));
+        }
+
+        [Test]
+        public void EfGenericRepository_ShouldCreateCarRepository_WhenThePassedParametersAreValid()
+        {
+            var context = new Mock<ICarsSystemDbContext>();
+
+            var repository = new EfGenericRepository<Car>(context.Object);
+
+            Assert.IsInstanceOf<EfGenericRepository<Car>>(repository);
+        }
+
+        [Test]
+        public void EfGenericRepository_ShouldCreateUserRepository_WhenThePassedParametersAreValid()
+        {
+            var context = new Mock<ICarsSystemDbContext>();
+
+            var repository = new EfGenericRepository<User>(context.Object);
+
+            Assert.IsInstanceOf<EfGenericRepository<User>>(repository);
+        }
+
+        [Test]
+        public void EfGenericRepository_ShouldCreateCarIRepository_WhenThePassedParametersAreValid()
+        {
+            var context = new Mock<ICarsSystemDbContext>();
+
+            var repository = new EfGenericRepository<Car>(context.Object);
+
+            Assert.IsInstanceOf<IRepository<Car>>(repository);
+        }
+
+        [Test]
+        public void EfGenericRepository_ShouldCreateUserIRepository_WhenThePassedParametersAreValid()
+        {
+            var context = new Mock<ICarsSystemDbContext>();
+
+            var repository = new EfGenericRepository<User>(context.Object);
+
+            Assert.IsInstanceOf<IRepository<User>>(repository);
+        }
+    }
+}
