3b29cbacf835c3a60af1edae82b9f82d585ca9b4
todor-enikov <enigmaenikov@gmail.com>
2017-02-15 11:40:59 +0200

    Added unit test for CarsService class.

---
+++ Source/CarsSystem.Tests/Services/CarsSystem.Services.Data/CarsServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using CarsSystem.Data.Repositories;
+using CarsSystem.Data;
+using CarsSystem.Data.Models;
+using CarsSystem.Services.Data;
+using CarsSystem.Services.Data.Contracts;
+
+namespace CarsSystem.Tests.Services.CarsSystem.Services.Data
+{
+    [TestFixture]
+    public class CarsServiceTests
+    {
+        [Test]
+        public void CarsService_ShouldThrowArgumentNullException_WhenPassedRepositoryIsNull()
+        {
+            //var data = new Mock<ICarsSystemDbContext>();
+            //var mockedRepository = new Mock<IRepository<Car>>();
+            IRepository<Car> mockedRepository = null;
+
+            Assert.Throws<ArgumentException>(() => new CarsService(mockedRepository));
+        }
+
+        [Test]
+        public void CarsService_ShouldThrowArgumentNullExceptionWithExpectedMessage_WhenPassedRepositoryIsNull()
+        {
+            IRepository<Car> mockedRepository = null;
+
+            var expectedMessage = Assert.Throws<ArgumentException>(() => new CarsService(mockedRepository));
+
+            Assert.IsTrue(expectedMessage.Message.Contains("null"));
+        }
+
+        [Test]
+        public void CarsService_ShouldCreateInstanceOfCarsService_WhenPassedRepositoryIsCorrectly()
+        {
+            var mockedRepository = new Mock<IRepository<Car>>();
+
+            var service = new CarsService(mockedRepository.Object);
+
+            Assert.IsInstanceOf<CarsService>(service);
+        }
+
+        [Test]
+        public void CarsService_ShouldImplementsInterfaceICarsService_WhenPassedRepositoryIsCorrectly()
+        {
+            var mockedRepository = new Mock<IRepository<Car>>();
+
+            var service = new CarsService(mockedRepository.Object);
+
+            Assert.IsInstanceOf<ICarsService>(service);
+        }
+    }
+}
