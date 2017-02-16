48d9b394e4718e1a0bbf46acebad10e168c27a6b
todor-enikov <enigmaenikov@gmail.com>
2017-02-16 16:48:21 +0200

    Added more unit tests for UserService class.

--- Source/CarsSystem.Tests/Services/CarsSystem.Services.Data/UserServiceTests.cs
+++ Source/CarsSystem.Tests/Services/CarsSystem.Services.Data/UserServiceTests.cs
@@ -5,6 +5,8 @@ using CarsSystem.Data.Repositories;
 using CarsSystem.Services.Data;
 using Moq;
 using CarsSystem.Services.Data.Contracts;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CarsSystem.Tests.Services.CarsSystem.Services.Data
 {
@@ -47,6 +49,68 @@ namespace CarsSystem.Tests.Services.CarsSystem.Services.Data
             var service = new UsersService(mockedRepository.Object);
 
             Assert.IsInstanceOf<IUsersService>(service);
+        }
+
+        [Test]
+        public void UserService_VerifyThehMethodGetAllCars_IsCalled_WhenPassedParametersAreCorrect()
+        {
+            var listOfUser = new List<User>
+            {
+                new User() { Id="test", FirstName="Gosho", LastName="Pochivkata" },
+                new User() { Id="test1", FirstName="Monti", LastName="Picha" },
+                new User() { Id="test2", FirstName="Marin", LastName="The hunter" },
+
+            };
+            var mockedService = new Mock<IUsersService>();
+            mockedService.Setup(c => c.GetAllUsers()).Returns(listOfUser);
+
+            var result = mockedService.Object.GetAllUsers();
+
+            mockedService.Verify(s => s.GetAllUsers(), Times.Exactly(1));
+        }
+
+        [Test]
+        public void UserService_VerifyThehMethodGetCarById_IsCalled_WhenPassedParametersAreCorrect()
+        {
+            var user = new User() { Id = "test", FirstName = "Gosho", LastName = "Pochivkata" };
+
+            var mockedService = new Mock<IUsersService>();
+            mockedService.Setup(c => c.GetUserById("test")).Returns(user);
+
+            var result = mockedService.Object.GetUserById("test");
+
+            mockedService.Verify(s => s.GetUserById("test"), Times.Exactly(1));
+        }
+
+        [Test]
+        public void UserService_GetUserByIdWorksProperly_WhenPassedParametersAreCorrect()
+        {
+            var user = new User() { Id = "test", FirstName = "Gosho", LastName = "Pochivkata" };
+
+            var mockedService = new Mock<IUsersService>();
+            mockedService.Setup(c => c.GetUserById("test")).Returns(user);
+
+            var result = mockedService.Object.GetUserById("test");
+
+            Assert.AreEqual(user.FirstName, result.FirstName);
+        }
+
+        [Test]
+        public void UserService_GetAllUsersMethodWorksProperly_WhenPassedParametersAreCorrect()
+        {
+            var listOfUser = new List<User>
+            {
+                new User() { Id="test", FirstName="Gosho", LastName="Pochivkata" },
+                new User() { Id="test1", FirstName="Monti", LastName="Picha" },
+                new User() { Id="test2", FirstName="Marin", LastName="The hunter" },
+
+            };
+            var mockedService = new Mock<IUsersService>();
+            mockedService.Setup(c => c.GetAllUsers()).Returns(listOfUser);
+
+            var result = mockedService.Object.GetAllUsers().ToList();
+
+            Assert.AreEqual(listOfUser.Count, result.Count);
         }
     }
 }
