5a2628b14480c9197ddc972e25770fdd6d849282
todor-enikov <enigmaenikov@gmail.com>
2017-02-16 23:21:19 +0200

    Added more unit tests for FilterService class.

--- Source/CarsSystem.Tests/Services/CarsSystem.Services.Data/FilterServiceTests.cs
+++ Source/CarsSystem.Tests/Services/CarsSystem.Services.Data/FilterServiceTests.cs
@@ -5,6 +5,8 @@ using CarsSystem.Services.Data;
 using CarsSystem.Services.Data.Contracts;
 using CarsSystem.Data.Repositories;
 using Moq;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CarsSystem.Tests.Services.CarsSystem.Services.Data
 {
@@ -47,6 +49,114 @@ namespace CarsSystem.Tests.Services.CarsSystem.Services.Data
             var service = new FilterService(mockedRepository.Object);
 
             Assert.IsInstanceOf<IFilterService>(service);
+        }
+
+        [Test]
+        public void FilterService_FilterExpiringVignetteCars_ShouldReturnIEnumerableCollection_WhenPassedParametersAreCorrect()
+        {
+            var collectionOfCars = new List<Car>
+            {
+                new Car() { Id=1, Manufacturer="VW", Model="Golf", ValidUntilVignette=DateTime.Now },
+                new Car() { Id=2, Manufacturer="BMW", Model="e40", ValidUntilVignette=DateTime.Now },
+                new Car() { Id=3, Manufacturer="Lada", Model="2105", ValidUntilVignette=DateTime.Now }
+            };
+
+            var mockedService = new Mock<IFilterService>();
+            mockedService.Setup(s => s.FilterExpiringVignetteCars()).Returns(collectionOfCars);
+
+            var result = mockedService.Object.FilterExpiringVignetteCars().ToList();
+
+            Assert.AreEqual(collectionOfCars.Count, result.Count);
+        }
+
+        [Test]
+        public void FilterService_FilterExpiringVignetteCars_VerifyThatTheMethodIsCalledExactOneTime_WhenPassedParametersAreCorrect()
+        {
+            var collectionOfCars = new List<Car>
+            {
+                new Car() { Id=1, Manufacturer="VW", Model="Golf", ValidUntilVignette=DateTime.Now },
+                new Car() { Id=2, Manufacturer="BMW", Model="e40", ValidUntilVignette=DateTime.Now },
+                new Car() { Id=3, Manufacturer="Lada", Model="2105", ValidUntilVignette=DateTime.Now }
+            };
+
+            var mockedService = new Mock<IFilterService>();
+            mockedService.Setup(s => s.FilterExpiringVignetteCars()).Returns(collectionOfCars);
+
+            var result = mockedService.Object.FilterExpiringVignetteCars();
+
+            mockedService.Verify(s => s.FilterExpiringVignetteCars(), Times.Exactly(1));
+        }
+
+        [Test]
+        public void FilterService_FilterExpiringInsurance_ShouldReturnIEnumerableCollection_WhenPassedParametersAreCorrect()
+        {
+            var collectionOfCars = new List<Car>
+            {
+                new Car() { Id=1, Manufacturer="VW", Model="Golf", ValidUntilInsurance=DateTime.Now },
+                new Car() { Id=2, Manufacturer="BMW", Model="e40", ValidUntilInsurance=DateTime.Now },
+                new Car() { Id=3, Manufacturer="Lada", Model="2105", ValidUntilInsurance=DateTime.Now }
+            };
+
+            var mockedService = new Mock<IFilterService>();
+            mockedService.Setup(s => s.FilterExpiringInsurance()).Returns(collectionOfCars);
+
+            var result = mockedService.Object.FilterExpiringInsurance().ToList();
+
+            Assert.AreEqual(collectionOfCars.Count, result.Count);
+        }
+
+        [Test]
+        public void FilterService_FilterExpiringInsurance_VerifyThatTheMethodIsCalledExactOneTime_WhenPassedParametersAreCorrect()
+        {
+            var collectionOfCars = new List<Car>
+            {
+                new Car() { Id=1, Manufacturer="VW", Model="Golf", ValidUntilInsurance=DateTime.Now },
+                new Car() { Id=2, Manufacturer="BMW", Model="e40", ValidUntilInsurance=DateTime.Now },
+                new Car() { Id=3, Manufacturer="Lada", Model="2105", ValidUntilInsurance=DateTime.Now }
+            };
+
+            var mockedService = new Mock<IFilterService>();
+            mockedService.Setup(s => s.FilterExpiringInsurance()).Returns(collectionOfCars);
+
+            var result = mockedService.Object.FilterExpiringInsurance();
+
+            mockedService.Verify(s => s.FilterExpiringInsurance(), Times.Exactly(1));
+        }
+
+        [Test]
+        public void FilterService_FilterExpiringAnnualCheckUp_ShouldReturnIEnumerableCollection_WhenPassedParametersAreCorrect()
+        {
+            var collectionOfCars = new List<Car>
+            {
+                new Car() { Id=1, Manufacturer="VW", Model="Golf", ValidUntilAnnualCheckUp=DateTime.Now },
+                new Car() { Id=2, Manufacturer="BMW", Model="e40", ValidUntilAnnualCheckUp=DateTime.Now },
+                new Car() { Id=3, Manufacturer="Lada", Model="2105", ValidUntilAnnualCheckUp=DateTime.Now }
+            };
+
+            var mockedService = new Mock<IFilterService>();
+            mockedService.Setup(s => s.FilterExpiringAnnualCheckUp()).Returns(collectionOfCars);
+
+            var result = mockedService.Object.FilterExpiringAnnualCheckUp().ToList();
+
+            Assert.AreEqual(collectionOfCars.Count, result.Count);
+        }
+
+        [Test]
+        public void FilterService_FilterExpiringAnnualCheckUp_VerifyThatTheMethodIsCalledExactOneTime_WhenPassedParametersAreCorrect()
+        {
+            var collectionOfCars = new List<Car>
+            {
+                new Car() { Id=1, Manufacturer="VW", Model="Golf", ValidUntilAnnualCheckUp=DateTime.Now },
+                new Car() { Id=2, Manufacturer="BMW", Model="e40", ValidUntilAnnualCheckUp=DateTime.Now },
+                new Car() { Id=3, Manufacturer="Lada", Model="2105", ValidUntilAnnualCheckUp=DateTime.Now }
+            };
+
+            var mockedService = new Mock<IFilterService>();
+            mockedService.Setup(s => s.FilterExpiringAnnualCheckUp()).Returns(collectionOfCars);
+
+            var result = mockedService.Object.FilterExpiringAnnualCheckUp();
+
+            mockedService.Verify(s => s.FilterExpiringAnnualCheckUp(), Times.Exactly(1));
         }
     }
 }
