0e74d3a477314d65ba99832c1e2131bf055a88f2
todor-enikov <enigmaenikov@gmail.com>
2017-02-17 16:06:11 +0200

    Implemented basic view for all cars.

--- Source/CarsSystem.WebForms.Client/Cars/AllCars.aspx
+++ Source/CarsSystem.WebForms.Client/Cars/AllCars.aspx
@@ -2,4 +2,15 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <h1>All cars page!</h1>
+    <asp:GridView ID="AllCarsGridView" runat="server" AutoGenerateColumns="false">
+        <Columns>
+            <asp:BoundField DataField="Manufacturer" HeaderText="Manufacturer"/>
+            <asp:BoundField DataField="Model" HeaderText="Model"/>
+            <asp:HyperLinkField 
+                    HeaderText="Registration number"
+                    DataTextField="RegistrationNumber"
+                    DataNavigateUrlFields="Id"
+                    DataNavigateUrlFormatString="~/Cars/CarDetails.aspx?id={0}"/>
+        </Columns>
+    </asp:GridView>
 </asp:Content>
--- Source/CarsSystem.WebForms.Client/Cars/AllCars.aspx.cs
+++ Source/CarsSystem.WebForms.Client/Cars/AllCars.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarsSystem.Services.Data.Contracts;
+using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,12 +11,18 @@ namespace CarsSystem.WebForms.Client.Cars
 {
     public partial class AllCars : System.Web.UI.Page
     {
+        [Inject]
+        public ICarsService Service { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!User.IsInRole("Admin"))
             {
                 Response.Redirect("~/ErrorPages/UnauthorizedAccess.aspx");
             }
+
+            this.AllCarsGridView.DataSource = Service.GetAllCars().ToList();
+            this.AllCarsGridView.DataBind();
         }
     }
 }
