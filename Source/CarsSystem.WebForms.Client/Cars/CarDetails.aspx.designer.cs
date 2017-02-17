30bcc45f6be7b92b454de6ee1234aaec25253e82
todor-enikov <enigmaenikov@gmail.com>
2017-02-17 16:32:25 +0200

    Finished CarDetails.aspx page.

--- Source/CarsSystem.WebForms.Client/Cars/CarDetails.aspx.designer.cs
+++ Source/CarsSystem.WebForms.Client/Cars/CarDetails.aspx.designer.cs
@@ -11,5 +11,122 @@ namespace CarsSystem.WebForms.Client.Cars {
     
     
     public partial class CarDetails {
+        
+        /// <summary>
+        /// ManufacturerLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ManufacturerLabel;
+        
+        /// <summary>
+        /// ModelLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ModelLabel;
+        
+        /// <summary>
+        /// TypeOfEngineLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label TypeOfEngineLabel;
+        
+        /// <summary>
+        /// RegistrationNumberLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label RegistrationNumberLabel;
+        
+        /// <summary>
+        /// VINLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label VINLabel;
+        
+        /// <summary>
+        /// CountOfTyresLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label CountOfTyresLabel;
+        
+        /// <summary>
+        /// CountOfDoorsLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label CountOfDoorsLabel;
+        
+        /// <summary>
+        /// TypeOfCarLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label TypeOfCarLabel;
+        
+        /// <summary>
+        /// YearOfManufactoringLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label YearOfManufactoringLabel;
+        
+        /// <summary>
+        /// AnnualCkeckLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label AnnualCkeckLabel;
+        
+        /// <summary>
+        /// VignetteLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label VignetteLabel;
+        
+        /// <summary>
+        /// InsuranceLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label InsuranceLabel;
+        
+        /// <summary>
+        /// CheckCarOwnerLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink CheckCarOwnerLabel;
     }
 }
