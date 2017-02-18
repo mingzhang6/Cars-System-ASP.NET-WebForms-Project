8ec22efde9daf5b4dd2221c24449ab689da49f1a
todor-enikov <enigmaenikov@gmail.com>
2017-02-18 19:28:28 +0200

    Implemented IMailService.

--- Source/Services/CarsSystem.Services.Data/Contracts/IMailService.cs
+++ Source/Services/CarsSystem.Services.Data/Contracts/IMailService.cs
@@ -8,5 +8,6 @@ namespace CarsSystem.Services.Data.Contracts
 {
     public interface IMailService
     {
+        void SendEmail(string subject, string content, IEnumerable<string> emails);
     }
 }
