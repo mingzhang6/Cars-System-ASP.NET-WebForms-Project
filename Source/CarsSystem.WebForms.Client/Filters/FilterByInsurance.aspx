dd0e5d8d66e8ef4a0cda84f9909b74f076c6796e
todor-enikov <enigmaenikov@gmail.com>
2017-02-18 19:40:34 +0200

    Finished FilterByInsurance page.

--- Source/CarsSystem.WebForms.Client/Filters/FilterByInsurance.aspx
+++ Source/CarsSystem.WebForms.Client/Filters/FilterByInsurance.aspx
@@ -1,5 +1,29 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" Title="Filter by insurance page" MasterPageFile="~/Site.Master" CodeBehind="FilterByInsurance.aspx.cs" Inherits="CarsSystem.WebForms.Client.Filters.FilterByInsurance" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h1>Filter by insurance page!</h1>
+    <div class="row">
+        <div class="col-md-9 col-md-offset-1">
+            <h1 class="text-center">All cars which insurance expires soon!</h1>
+            <asp:GridView ID="FilterGridView" runat="server" AutoGenerateColumns="false" CssClass="col-md-offset-2 text-center">
+                <Columns>
+                    <asp:BoundField DataField="Manufacturer" HeaderText="Manufacturer" />
+                    <asp:BoundField DataField="Model" HeaderText="Model" />
+                    <asp:BoundField DataField="RegistrationNumber" HeaderText="Registration number" />
+                    <asp:BoundField DataField="VINNumber" HeaderText="VIN number" />
+                    <asp:BoundField DataField="ValidUntilAnnualCheckUp" HeaderText="Valid until" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-md-12 col-md-offset-2">
+            <asp:Label ID="EmailSubjectLabel" runat="server" Text="Subject: "></asp:Label>
+            <asp:TextBox ID="EmailSubjectTextBox" runat="server"></asp:TextBox>
+            <asp:Label ID="EmailContentLabel" runat="server" Text="Content:"></asp:Label>
+            <asp:TextBox ID="EmailContentBox" runat="server"></asp:TextBox>
+            <asp:Button ID="SendMailButton" runat="server" Text="Send e-mails to the filtered cars!" OnClick="SendMailButton_Click" />
+        </div>
+    </div>
 </asp:Content>
--- Source/CarsSystem.WebForms.Client/Filters/FilterByInsurance.aspx.designer.cs
+++ Source/CarsSystem.WebForms.Client/Filters/FilterByInsurance.aspx.designer.cs
@@ -11,5 +11,59 @@ namespace CarsSystem.WebForms.Client.Filters {
     
     
     public partial class FilterByInsurance {
+        
+        /// <summary>
+        /// FilterGridView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView FilterGridView;
+        
+        /// <summary>
+        /// EmailSubjectLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EmailSubjectLabel;
+        
+        /// <summary>
+        /// EmailSubjectTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EmailSubjectTextBox;
+        
+        /// <summary>
+        /// EmailContentLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EmailContentLabel;
+        
+        /// <summary>
+        /// EmailContentBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EmailContentBox;
+        
+        /// <summary>
+        /// SendMailButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SendMailButton;
     }
 }
